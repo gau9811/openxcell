bd3f0fcbcbc097af1afa962f50977ae686ac4b5b
Gaurav__bajaj <44838878+gau9811@users.noreply.github.com>
2021-05-28 17:36:35 +0530

    Add files via upload

---
+++ design/testdesign/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+     <div className="header">
+      <p>Title</p> 
+     </div>
+     <div className="content">
+     <div className="main-square-container">
+       <div className="square">
+         <ul className="square-ul">
+           <li>Name</li>
+           <li>leasdmfg.adsfg.adg.afdg.fadg</li>
+           <li>Namadfg.adfg.adfg.adf.ge</li>
+         </ul>
+       </div>
+       <div className="square">
+         <ul className="square-ul">
+           <li>Name</li>
+           <li>leasdmfg.adsfg.adg.afdg.fadg</li>
+           <li>Namadfg.adfg.adfg.adf.ge</li>
+         </ul>
+       </div>
+       <div className="square">
+         <ul className="square-ul">
+           <li>Name</li>
+           <li>leasdmfg.adsfg.adg.afdg.fadg</li>
+           <li>Namadfg.adfg.adfg.adf.ge</li>
+         </ul>
+       </div>
+</div>
+
+
+     </div>
+     <div className="footer">
+      <p> footer</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
